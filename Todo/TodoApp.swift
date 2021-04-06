74ba151a03a7ebbda6d30ec5a60f6814c787d538
Ameer Hamza <hamza@edkasa.com>
2021-04-06 12:39:12 +0500

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Ameer Hamza on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
